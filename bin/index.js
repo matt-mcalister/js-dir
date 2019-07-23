61fc8b4735a04aac3102637550809f25c5ff9da1
Matt McAlister <32518899+matt-mcalister@users.noreply.github.com>
2019-07-23 17:04:21 -0400

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-const generate_js_dir = require('../') /* the current working directory so that means main.js because of package.json */
+const generate_js_dir = require('../')
 let dir_name = process.argv[2] ? process.argv[2] : "js-test-suite"
 generate_js_dir(dir_name)
